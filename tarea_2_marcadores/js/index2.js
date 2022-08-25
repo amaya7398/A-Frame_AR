457c09fc7406f48c96d204e04b8eea9a8639d8f8
Daniel S Amaya <daniel_amaya7398@hotmail.com>
2022-08-25 12:46:21 -0500

    New: creation random boxes aroud a 'hiro' mark

--- tarea_2_marcadores/js/index2.js
+++ tarea_2_marcadores/js/index2.js
@@ -8,8 +8,28 @@ boxes[4] = "<a-box height=0.25 width=0.25 depth=0.25 position='-0.5 0 0' color='
 boxes[5] = "<a-box height=0.25 width=0.25 depth=0.25 position='0 0 0.5'  color='#4CC3D9'></a-box>";
 boxes[6] = "<a-box height=0.25 width=0.25 depth=0.25 position='0 0 -0.5'  color='#4CC3D9'></a-box>";
 
+boxes[9] = "<a-box height=0.25 width=0.25 depth=0.25 position='0 0 4'  color='#4CC3D9'></a-box>";
+const nRandomNumbers = 100;
+
+const getRandomNumbers = () => {
+    const xPos = (Math.random() * 8) - 4; // [-4, 4]
+    const yPos = (Math.random() * 8) - 4; // [-4, 4]
+    const zPos = (Math.random() * 8) - 4; // [-4, 4]
+    return { xPos, yPos, zPos };
+}
+
+const get_a_box = () => {
+    const {xPos, yPos, zPos} = getRandomNumbers();
+    return `<a-box height=0.25 width=0.25 depth=0.25 position='${xPos} ${yPos} ${zPos}'  color='#4CC3D9'></a-box>`
+}
+
+for (let i = 0; i < nRandomNumbers; i++) {
+    random.innerHTML += get_a_box();
+}
+
 // boxes.forEach(box =>{
 //     random.innerHTML += box;
 // })
-random.innerHTML = boxes[0] + boxes[1] + boxes[2] + boxes[3] + boxes[4] + boxes[5] + boxes[6];
-console.log(random)
+
+// random.innerHTML += boxes[9]
+// console.log(random)
