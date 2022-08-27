25eea849b28bff9689fe096b6f464708347379d5
Daniel S Amaya <daniel_amaya7398@hotmail.com>
2022-08-27 10:34:32 -0500

    Creacion 2 entidades
    ToDo:
    	* .emit() in one marker
    	* event listener in marker collider

--- tarea_2_marcadores/tarea2.js
+++ tarea_2_marcadores/tarea2.js
@@ -1,54 +1,32 @@
 let sceneEl = document.querySelector('a-scene');
 let marker = document.querySelector('a-marker');
 
-AFRAME.registerComponent("marker-dos", {
-
-    schema: {
-        // position:{type: "number", default: 0},
-        form:{type: "number", default: 5, position: {x:1, y:2, z:3}},
-    },
-    init: function () {
-        // Do something when component first attached.
-        //"el" es el componente a crear this.el
-        this.el.object3D.position.set(3, 3, 3);
-        this.el.object3D.position.x += 1;
-        console.log("dosINIT ####################");
-        // console.log("dos",this.el)
-        console.log("dosObject3D",this.el.object3D)
-        console.log("dosObject3D.Position",this.el.object3D.position)
-        // console.log("dos",this.el.sceneEl)
-        // console.log("dos",this.el.market)
-    },
-    tick: function (time, timeDelta) {
-        // Do something on every scene tick or frame.
-        // console.log("tickDos")
-        // console.log("dos",this.el.market)
-        // console.log("In2-POs",this.el.getAttribute("position").x)
-    }
-});
-
 AFRAME.registerComponent("marker-handler", {
 
     schema: {
-        // position:{type: "number", default: 0},
-        form:{type: "number", default: 0}
+        width : {type: "number", default: 0.5},
+        height : {type: "number", default: 0.5},
+        depth : {type: "number", default: 0.5},
+        color : {type: "color", default: "#ACBD5E"},
     },
     init: function () {
-        // Do something when component first attached.
-        console.log("handlerINIT ####################")
-        // console.log("handler",this.el)
-        console.log("handler",this.el.object3D)
-        console.log("handlerPosition",this.el.object3D.position)
-        // console.log("handler",this.el.sceneEl)
-        // console.log("handler",this.el.marker)
+        console.log("MARKER-HANDLER ####################")
+        // console.log("handler", this.el.object3D)
+        // console.log("handlerPosition", this.el.object3D.position)
+
+        const {data} = this
+        // Create geometry
+        this.geometry = new THREE.BoxGeometry(data.width, data.height, data.depth);
+        // Create material
+        this.material = new THREE.MeshStandardMaterial({ color: data.color });
+        //create mesh
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        
+        //Set mesh on entity
+        this.el.setObject3D("mesh", this.mesh);
     },
-    update: function () {
-    // Do something when component's data is updated.
-        // console.log("UPDATE COMPONENT")
-    },
-    remove: function () {
-        // Do something the component or its entity is detached.
-        // console.log("REMOVE COMPONENT")
+    update : function (oldData) {
+        console.log("Handler event listener",this.el)
     },
     tick: function (time, timeDelta) {
         // Do something on every scene tick or frame.
@@ -56,4 +34,52 @@ AFRAME.registerComponent("marker-handler", {
         // console.log("dos",this.el.market)
         // console.log("In-POs",this.el.getAttribute("position").x)
     }
-})
+});
+
+AFRAME.registerComponent("marker-grown", {
+
+    schema: {
+        width : {type: "number", default: 0.5},
+        height : {type: "number", default: 0.5},
+        depth : {type: "number", default: 0.5},
+        color : {type: "color", default: "#4CC3D9"},
+    },
+    init: function () {
+        console.log("MARKER-GROWN ####################");
+        // this.el.object3D.position.set(3, 3, 3);
+        // this.el.object3D.position.z += 1;
+
+        const {data} = this
+        // Create geometry
+        this.geometry = new THREE.BoxGeometry(data.width, data.height, data.depth);
+        // Create material
+        this.material = new THREE.MeshStandardMaterial({ color: data.color });
+        //create mesh
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        
+        //Set mesh on entity
+        this.el.setObject3D("mesh", this.mesh);
+    },
+    update: function(oldData){
+        const data = this.data
+        //Update also activate in "init", so we dont want UPDATE if it happens
+        // console.log(data)
+        // console.log(oldData)
+
+        // if (Object.keys(oldData).length === 0) { return }
+        // console.log(this.mesh.geometry.parameters)
+        // this.mesh.geometry.parameters.width = 0
+        // console.log(this.mesh.geometry.parameters)
+    },
+    tick: function (time, timeDelta) {
+        // Do something on every scene tick or frame.
+        const {data} = this
+        if(this.data.width < 5){
+            data.width = data.width+0.1;
+            data.height = data.height+0.1;
+            data.depth = data.depth+0.1;
+            // this.el.getObject3D("mesh").geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
+            // this.data = {...data, width: data.width+0.01, height :data.height+0.01, depth : data.depth+0.01}
+        }
+    }
+});
