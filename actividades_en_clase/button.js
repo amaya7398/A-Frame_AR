0f471fdacfc43d97650cd35cedddc57e6b7e9256
Daniel S Amaya <daniel_amaya7398@hotmail.com>
2022-09-02 15:44:28 -0500

    Update: manipular una circulo con 2 marcadores
    Dado dos marcadores:
    	Se posiciona una esfera en su punto medio.
    	Se redimensiona con referencia a la distancia entre estos dos

--- actividades_en_clase/button.js
+++ actividades_en_clase/button.js
@@ -1,36 +1,52 @@
 // import this on your HTML
 window.addEventListener('load', () => {
-    const camera = document.querySelector('[camera]');
-    const marker = document.querySelector('a-marker');
-    let check;
+  const camera = document.querySelector('[camera]');
+  const kanjiMarker = document.querySelector('#kanji');
+  const hiroMarker = document.querySelector('#hiro');
+  const planet = document.querySelector("#planet");
 
-    document.querySelector(".say-hi-button")
+  let check;
+
+  document.querySelector("#a")
     .addEventListener("click", function () {
-      alert("Hi there!");
+      takeDistance()
     });
-    document.querySelector(".say-bye-button")
-    .addEventListener("click", ()=>{
-        alert("Bye there");
+  document.querySelector("#b")
+    .addEventListener("click", () => {
+      quitarCheck();
     })
 
+  // kanjiMarker.addEventListener('markerFound', () => {
+  const takeDistance = () => {
+    check = setInterval(() => {
+      resizeCircle()
 
+      // do what you want with the distance:
+    }, 500);
+  };
 
-    marker.addEventListener('markerFound', () => {
-        let cameraPosition = camera.object3D.position;
-        let markerPosition = marker.object3D.position;
-        let distance = cameraPosition.distanceTo(markerPosition)
+  const resizeCircle = () => {
+      const markerHiroPosition = hiroMarker.object3D.position;
+      const markerKanjiPosition = kanjiMarker.object3D.position;
+      const distance = markerHiroPosition.distanceTo(markerKanjiPosition)
 
-        check = setInterval(() => {
-            cameraPosition = camera.object3D.position;
-            markerPosition = marker.object3D.position;
-            distance = cameraPosition.distanceTo(markerPosition)
+    // planet.setAttribute("geometry", { radius: distance / 2 });
+    moveMidPointBetween2Markers();
+  }
 
-            // do what you want with the distance:
-            console.log(distance, " <=>", marker);
-        }, 100);
-    });
+  const moveMidPointBetween2Markers = (markerHiroPosition, mar) => {
+    planet.setAttribute("position", calculateMidPoint());
+  }
 
-    marker.addEventListener('markerLost', () => {
-      clearInterval(check);
-    })
+  const calculateMidPoint = () => {
+    const {x:x1,y:y1,z:z1} = hiroMarker.object3D.position;
+    const {x:x2, y:y2, z:z2} = kanjiMarker.object3D.position;
+
+    return {x:(x1+x2)/2, y:(y1+y2)/2, z:(z1+z2)/2}
+  }
+
+  //markerLost
+  const quitarCheck = () => {
+    clearInterval(check);
+  }
 })
